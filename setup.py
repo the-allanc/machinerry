49ad888c47405fc7f6fcab654288c08e1f8327c5
Allan Crooks <allan@increment.one>
2018-06-18 01:12:02 +0100

    Add pytest-cov as a test dependency.

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ params = dict(
             'pytest>=3.5',
             'pytest-sugar>=0.9.1',
             'collective.checkdocs',
+            'pytest-cov',
         ],
         'docs': [
             'sphinx',
